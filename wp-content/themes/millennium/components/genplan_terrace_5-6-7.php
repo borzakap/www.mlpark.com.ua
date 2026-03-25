f740d120aac5e6f9ffcf34fc2fd15421e6e386ee
oleksiiborzak <borzakap@gmail.com>
2026-03-25 17:18:13 +0200

    Add genplan component for terrace page and improve code formatting

---
+++ wp-content/themes/millennium/components/genplan_terrace_5-6-7.php
@@ -0,0 +1,3 @@
+<div class="genplan">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/genplan/terras_genplan_3b.png" alt="genplan-img" class="img-fluid" loading="lazy" width="1600" height="778">
+</div>
